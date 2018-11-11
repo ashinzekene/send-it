49328a46a93fc55b70b0e7a1118f4d1a4a1f78ad
ashinzekene <ashinzekene@gmail.com>
2018-11-11 21:15:26 +0100

    implemented auth route

--- test/users.js
+++ test/users.js
@@ -9,38 +9,46 @@ const testUser = {
   firstname: 'Ekene',
   lastname: 'Ekonash',
   othername: 'Eky',
-  email: 'ekonash28@gmail.com',
+  email: 'ekonash1@gmail.com',
 }
+
+let id;
+let token;
 
 const URL_PREFIX = '/api/v1/'
 const chaiReq = chai.request(server).keepOpen();
 const expect = chai.expect;
 
-
 describe('USERS', () => {
+  after(async () => {
+    const { body } = await chaiReq.post(`${URL_PREFIX}drop/users`).send();
+  });
+
+  it(`POST ${URL_PREFIX}auth/signup Should create a user`, async () => {
+    const { body, status } = await chaiReq.post(`${URL_PREFIX}auth/signup`).send(testUser);
+    id = body.data.id;
+    expect(status).to.equal(200);
+    expect(body.data).to.include.all.keys('firstname', 'lastname', 'email');
+  });
+  
   it(`GET ${URL_PREFIX} Should get all users`, async () => {
     const { body, status } = await chaiReq.get(`${URL_PREFIX}users`)
     expect(status).to.equal(200);
-    expect(body).to.be.an('array');
+    expect(body.data).to.be.an('array');
   });
   
   it(`GET ${URL_PREFIX}users/:id Should get a single user by id`, async () => {
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}users/1`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}users/${id}`)
     expect(status).to.equal(200);
-    expect(body).to.include.all.keys('firstname', 'lastname', 'email');
+    expect(body.data).to.include.all.keys('firstname', 'lastname', 'email');
   });
 
-  it(`POST ${URL_PREFIX} Should create a user`, async () => {
-    const { body, status } = await chaiReq.post(`${URL_PREFIX}users`).send(testUser);
-    expect(status).to.equal(200);
-    expect(body).to.include.all.keys('firstname', 'lastname', 'email');
-  });
-
-  it(`POST ${URL_PREFIX} Should create a user and return a token`, async () => {
+  it(`POST ${URL_PREFIX}auth/signup Should create a user and return a token`, async () => {
     const testUser2 = { ...testUser, email: 'ekonash28@gmail.com' };
-    const { body, status } = await chaiReq.post(`${URL_PREFIX}users`).send(testUser);
+    const { body, status } = await chaiReq.post(`${URL_PREFIX}auth/signup`).send(testUser2);
+    token = body.data.token;
     expect(status).to.equal(200);
-    expect(body).to.include.all.keys('firstname', 'lastname', 'email', 'token');
+    expect(body.data).to.include.all.keys('firstname', 'lastname', 'email', 'token');
   });
 
 })
