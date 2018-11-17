2ab21666070125b33b04f11c49c461e5e82c7cfe
ashinzekene <ashinzekene@gmail.com>
2018-11-17 06:36:37 +0100

    allowed only parcel creator change destination

--- test/parcel.js
+++ test/parcel.js
@@ -10,47 +10,79 @@ const testUser = {
   othername: 'Eky',
   password: 'mypassword',
   email: 'ekonash2@gmail.com',
-}
+};
 const testParcel = {
   weight: '23',
   to: 'Lagos',
   from: 'Abuja',
-}
+};
 
 
-const URL_PREFIX = '/api/v1/'
-const chaiReq =  chai.request(server).keepOpen();
+const URL_PREFIX = '/api/v1/';
+const chaiReq = chai.request(server).keepOpen();
 const { expect } = chai;
-let id = 1;
+let parcelId = 1;
+let userToken;
 
 describe('PARCELS', () => {
-  after(async() => {
-    await chaiReq.post(`${URL_PREFIX}drop/parcels`).send();    
-    await chaiReq.post(`${URL_PREFIX}drop/users`).send();    
-  })
+  after(async () => {
+    await chaiReq.post(`${URL_PREFIX}drop/parcels`).send();
+    await chaiReq.post(`${URL_PREFIX}drop/users`).send();
+  });
 
   it(`POST ${URL_PREFIX}parcels Should create a parcel`, async () => {
     const { body: userBody } = await chaiReq.post(`${URL_PREFIX}auth/signup`).send(testUser);
-    const token = userBody.data[0].token;
+    userToken = userBody.data[0].token;
     const { body, status } = await chaiReq.post(`${URL_PREFIX}parcels`)
-    .set('Authorization', `Bearer ${token}`)    
-    .send(testParcel);
-    id = body.data[0].id;
+      .set('Authorization', `Bearer ${userToken}`)
+      .send(testParcel);
+    parcelId = body.data[0].id;
     expect(status).to.equal(200);
     expect(body.data[0]).to.include.all.keys('weight', 'from', 'to');
   });
-  
+
   it(`GET ${URL_PREFIX}parcels/:id Should get a single parcel by id`, async () => {
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}parcels/${id}`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}parcels/${parcelId}`);
     expect(status).to.equal(200);
     expect(body.data[0]).to.include.all.keys('weight', 'from', 'to');
-    expect(body.data[0]).to.have.property('from', testParcel.from);    
+    expect(body.data[0]).to.have.property('from', testParcel.from);
   });
-  
+
   it(`GET ${URL_PREFIX}parcels Should get all parcels`, async () => {
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}parcels`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}parcels`);
     expect(status).to.equal(200);
     expect(body.data[0]).to.include.all.keys('weight', 'from', 'to', 'senton');
   });
 
-})
+  it(`PATCH ${URL_PREFIX}parcels/:parcel/destination Should not allow unauth user change destination`, async () => {
+    const { body, status } = await chaiReq.patch(`${URL_PREFIX}parcels/${parcelId}/destination`).send({
+      to: 'PortHarcourt',
+    });
+    expect(status).to.eql(401);
+    expect(body.error).to.include('No Authorization');
+  });
+
+  it(`PATCH ${URL_PREFIX}parcels/:parcel/destination Should allow only parcel creator change destination`, async () => {
+    const fakeUser = { ...testUser, email: 'fakeemail@gmail.com' };
+    const { body: userBody } = await chaiReq.post(`${URL_PREFIX}auth/signup`).send(fakeUser);
+    const fakeToken = userBody.data[0].token;
+    const { body, status } = await chaiReq.patch(`${URL_PREFIX}parcels/${parcelId}/destination`)
+      .set('Authorization', `Bearer ${fakeToken}`)
+      .send({
+        to: 'PortHarcourt',
+      });
+    expect(status).to.eql(401);
+    expect(body.error).to.eql('Unauthorized. Only the creator of this order can do that');
+  });
+
+  it(`PATCH ${URL_PREFIX}parcels/:parcel/destination Should update parcel destination with parcel creator`, async () => {
+    const { body, status } = await chaiReq.patch(`${URL_PREFIX}parcels/${parcelId}/destination`)
+      .set('Authorization', `Bearer ${userToken}`)
+      .send({
+        to: 'PortHarcourt',
+      });
+    console.log(body.error);
+    expect(status).to.eql(200);
+    // expect(body.error).to.include('No authorization');
+  });
+});
--- test/users.js
+++ test/users.js
@@ -1,7 +1,6 @@
 import chai from 'chai';
-import chaiHttp from 'chai-http'
-import config from './index';
-import server from '../lib/app'
+import chaiHttp from 'chai-http';
+import server from '../lib/app';
 
 chai.use(chaiHttp);
 
@@ -11,27 +10,27 @@ const testUser = {
   othername: 'Eky',
   password: 'mypassword',
   email: 'ekonash1@gmail.com',
-}
+};
 const testParce1l = {
   weight: '23',
   to: 'Lagos',
   from: 'Abuja',
-}
+};
 const testParcel2 = {
   weight: '23',
   to: 'Lagos',
   from: 'Abuja',
-}
+};
 const testParcel3 = {
   weight: '23',
   to: 'Lagos',
   from: 'Abuja',
-}
+};
 
-let id;
+let userId;
 let token;
 
-const URL_PREFIX = '/api/v1/'
+const URL_PREFIX = '/api/v1/';
 const chaiReq = chai.request(server).keepOpen();
 const { expect } = chai;
 
@@ -42,52 +41,52 @@ describe('USERS', () => {
 
   it(`POST ${URL_PREFIX}auth/signup Should create a user`, async () => {
     const { body, status } = await chaiReq.post(`${URL_PREFIX}auth/signup`).send(testUser);
-    id = body.data[0].user.id;
+    userId = body.data[0].user.id;
     expect(status).to.equal(200);
     expect(body.data[0].user).to.include.all.keys('firstname', 'lastname', 'email');
   });
-  
+
   it(`POST ${URL_PREFIX}auth/signup Should create a user and return a token`, async () => {
     const testUser2 = { ...testUser, email: 'ekonash28@gmail.com' };
     const { body, status } = await chaiReq.post(`${URL_PREFIX}auth/signup`).send(testUser2);
     token = body.data[0].token;
-    id = body.data[0].user.id;
+    userId = body.data[0].user.id;
     expect(status).to.equal(200);
     expect(body.data[0]).to.include.all.keys('token', 'user');
-    expect(body.data[0].user).to.include.all.keys('firstname', 'lastname', 'email',);
+    expect(body.data[0].user).to.include.all.keys('firstname', 'lastname', 'email');
   });
 
   it(`GET ${URL_PREFIX} Should get all users`, async () => {
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}users`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}users`);
     expect(status).to.equal(200);
     expect(body.data).to.be.an('array');
   });
-  
+
   it(`GET ${URL_PREFIX}users/:id Should get a single user by id`, async () => {
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}users/${id}`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}users/${userId}`);
     expect(status).to.equal(200);
     expect(body.data[0].user).to.include.all.keys('firstname', 'lastname', 'email');
   });
 
   it(`GET ${URL_PREFIX}auth/ Should get an already signed in user with token`, async () => {
     const { body, status } = await chaiReq
-    .get(`${URL_PREFIX}auth/`)
-    .set('Authorization', `Bearer ${token}`)
+      .get(`${URL_PREFIX}auth/`)
+      .set('Authorization', `Bearer ${token}`);
     expect(status).to.equal(200);
     expect(body.data[0].user).to.include.all.keys('firstname', 'lastname', 'email');
     expect(body.data[0].user).to.have.property('email', 'ekonash28@gmail.com');
   });
 
   it(`GET ${URL_PREFIX}auth/ Should show unauthorized without auth header`, async () => {
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}auth/`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}auth/`);
     expect(status).to.equal(401);
     expect(body.status).to.eql(401);
     expect(body.error).to.include('Authorization');
   });
-  
+
   it(`POST ${URL_PREFIX}auth/login login correctly`, async () => {
     const { body, status } = await chaiReq.post(`${URL_PREFIX}auth/login`)
-    .send({ email: testUser.email, password: testUser.password });
+      .send({ email: testUser.email, password: testUser.password });
     expect(status).to.equal(200);
     expect(body.data[0].user).to.include.all.keys('firstname', 'lastname', 'email');
     expect(body.data[0].user).to.have.property('email', testUser.email);
@@ -95,7 +94,7 @@ describe('USERS', () => {
 
   it(`POST ${URL_PREFIX}auth/login invalid credentials should fail`, async () => {
     const { body, status } = await chaiReq.post(`${URL_PREFIX}auth/login`)
-    .send({ email: testUser.email, password: 'testUser.password' });
+      .send({ email: testUser.email, password: 'testUser.password' });
     expect(status).to.equal(401);
     expect(body.status).to.equal(401);
     expect(body.error).to.include('Password');
@@ -103,15 +102,18 @@ describe('USERS', () => {
 
   it(`GET ${URL_PREFIX}users/:user/parcels Should fetch users parcels`, async () => {
     const createParcel = parcel => chaiReq.post(`${URL_PREFIX}parcels`)
-    .set('Authorization', `Bearer ${token}`)    
-    .send(parcel);
-    const b = await Promise.all([createParcel(testParce1l), createParcel(testParcel2), createParcel(testParcel3)]);
+      .set('Authorization', `Bearer ${token}`)
+      .send(parcel);
+    const b = await Promise.all([
+      createParcel(testParce1l),
+      createParcel(testParcel2),
+      createParcel(testParcel3)]);
     const parcels = b.map(({ body }) => body.data[0]);
-    const { body, status } = await chaiReq.get(`${URL_PREFIX}users/${id}/parcels`)
+    const { body, status } = await chaiReq.get(`${URL_PREFIX}users/${userId}/parcels`);
     expect(status).to.equal(200);
     expect(body.status).to.eql(200);
     expect(body.data[0].from).to.eql(testParce1l.from);
     expect(body.data[0].to).to.eql(testParce1l.to);
     expect(body.data.length).to.eql(parcels.length);
   });
-})
+});
